4b95a32c92454e245d7868b76eee81b2b46c5606
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2021-02-26 14:15:22 +0100

    Update docs example

--- gen-docs.js
+++ gen-docs.js
@@ -7,6 +7,8 @@ const config = {
   srcDir: "./src",
   title: "Hyper | API Docs",
   lang: "en",
+  projectName: 'Hyper',
+  issuesUrl: 'www.com'
 };
 
 const path = require("path");
@@ -36,18 +38,88 @@ int DeprecatedBar;
 
 (async () => {
   const files = [];
-  for await (const f of getFiles(config.srcDir)) {
-    files.push(f);
-    const fileData = (await fs.readFile(f, { encoding: "utf8" })).split("\n");
+  for await (const filePath of getFiles(config.srcDir)) {
+    const fileData = (await fs.readFile(filePath, { encoding: "utf8" })).split("\n");
+    files.push({
+      /** @type {string} */
+      path: filePath,
+      /** @type {string[]} */
+      data: fileData,
+    });
     // const res = parseSource(fileData);
   }
-  const res = parseSource(testSrc);
 
   const htmlTemplate = `
 ${startHtml}
-  # Files
 
-${files.map((filePath) => "- " + filePath).join("\n")}
+![](./media/banner.png)
+
+# Hyped <span style="color:lightgray"> - Editor of the future, today.</span>
+
+Brief project description.
+
+## Files
+
+${files.map((filePath) => "- **" + filePath.path.replace('C:\\dev\\hyped\\src\\', '') + '** - Pippo').join("\n")}
+
+## FooModule <span style="color:lightgray">#include "foo_file.c"</span>
+
+FooModule brief description.
+
+### internal void FooNew(); ![](https://img.shields.io/badge/-new_v1.0-green?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+### internal void FooDeprecated(); ![](https://img.shields.io/badge/-deprecated-red?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+!!! ERROR: Deprecated
+    This function is deprecated, use ... instead.
+
+## FooModule <span style="color:lightgray">#include "foo_file.c"</span>
+
+FooModule brief description.
+
+### internal void FooNew(); ![](https://img.shields.io/badge/-new_v1.0-green?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+### internal void FooDeprecated(); ![](https://img.shields.io/badge/-deprecated-red?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+!!! ERROR: Deprecated
+    This function is deprecated, but a replacement was not provided. Please open an issue [here](${config.issuesUrl}).
+
+## FooModule <span style="color:lightgray">#include "foo_file.c"</span>
+
+FooModule brief description.
+
+### internal void FooNew(); ![](https://img.shields.io/badge/-new_v1.0-green?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+### internal void FooDeprecated(); ![](https://img.shields.io/badge/-deprecated-red?style=plastic)
+
+Brief description of this function.
+
+More exhaustive description.
+
+!!! ERROR: Deprecated
+    This function is deprecated, but a replacement was not provided. Please open an issue [here](${config.issuesUrl}).
+
+### Demo
 
 !!!
     I'm a note. Don't mind me, I'm just sitting here.
@@ -66,12 +138,14 @@ ${files.map((filePath) => "- " + filePath).join("\n")}
 
 ${endHtml}`;
 
-  // fs.writeFile("docs.html", htmlTemplate, { encoding: "utf8" });
-
-  // console.log(htmlTemplate);
+  fs.writeFile(`${config.projectName}.docs.html`, htmlTemplate, { encoding: "utf8" });
 })();
 
 
+/**
+ * 
+ * @param {*} source 
+ */
 function parseSource(source) {
   for (const line of source.split("\n")) {
     if (line.indexOf("///") >= 0) {
